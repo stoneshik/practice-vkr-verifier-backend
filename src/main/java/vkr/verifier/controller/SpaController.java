aab885fe7983ba4d77afe12ba299386a2135ed65
stoneshik <strebilly@mail.ru>
2026-03-29 22:13:08 +0300

    добавил фронтенд

---
+++ src/main/java/vkr/verifier/controller/SpaController.java
@@ -0,0 +1,16 @@
+package vkr.verifier.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SpaController {
+    @GetMapping({
+        "/",
+        "/{path:^(?!api$)[^\\.]*}",
+        "/**/{path:^(?!api$)[^\\.]*}"
+    })
+    public String forwardToIndex() {
+        return "forward:/index.html";
+    }
+}
